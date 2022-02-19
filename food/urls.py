41671d18429d3956538d985d4a23658334905564
kamlesh <kishan217.kk@gmail.com>
2022-02-19 16:31:32 +0530

    item database connection and form added

--- food/urls.py
+++ food/urls.py
@@ -4,7 +4,8 @@ from food import views
 urlpatterns = [
     path('',views.home, name='home'),
     path('contact/',views.contact, name='contact'),
-    path('admin/',views.admin, name='admin'),
+    path('create_item/',views.create_item, name='admin'),
+    #path('add/',views.create_item, name='create_item'),
 ]
 
 
--- food/views.py
+++ food/views.py
@@ -19,6 +19,48 @@ def contact(request):
     return render(request,'foods/contact.html' )
 
 
+def create_item(request):
+
+    if request.method=='POST':
+        name=request.POST['name']
+        type=request.POST['type']
+        des=request.POST['desc']
+        price=request.POST['price']
+        image=request.POST['image']
+
+        if type=='lunch':
+            if image=='':
+                lunch_obj=Lunch(item_name=name, item_desc=des, item_price=price)
+                lunch_obj.save()
+
+            else:
+                lunch_obj=Lunch(item_name=name, item_desc=des, item_price=price, item_image=image)
+            lunch_obj.save()
+
+        elif type=='dinner':
+            if image=='':
+                dinner_obj=dinner(item_name=name, item_desc=des, item_price=price)
+                dinner_obj.save()
+
+            else:
+                dinner_obj=dinner(item_name=name, item_desc=des, item_price=price, item_image=image)
+            dinner_obj.save()
+          
+        
+        elif type=='breakfast':
+            if image=='':
+                breakfast_obj=Breakfast(item_name=name, item_desc=des, item_price=price)
+                breakfast_obj.save()
+             
+
+            else:
+                breakfast_obj=Breakfast(item_name=name, item_desc=des, item_price=price, item_image=image)
+            breakfast_obj.save()
+         
+
+    return render(request,'foods/item.html' )
+
 
 def admin(request):
     return redirect('admin') 
+    
