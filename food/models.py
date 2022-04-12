f91366e599b2686c44bba202f3df84af353762dd
kamlesh <kishan217.kk@gmail.com>
2022-04-13 00:02:39 +0530

    reservation added

--- food/models.py
+++ food/models.py
@@ -3,3 +3,15 @@ from django.db import models
 # Create your models here.
 class Gallery(models.Model):
     gallery_image=models.ImageField( upload_to="gallery/")
+
+class Item(models.Model):
+    category_list=(
+        ('B', 'Breakfast'),
+        ('L','Lunch'),
+        ('D','Dinner')
+    )
+    title=models.CharField( max_length=100)
+    description=models.TextField()
+    price=models.FloatField()
+    image=models.ImageField(upload_to='Item_folder/')
+    category=models.CharField(max_length=1, choices=category_list)
--- food/views.py
+++ food/views.py
@@ -1,5 +1,9 @@
+from cmath import exp
+from multiprocessing import context
+from unicodedata import category
+from django.conf import settings
 from django.shortcuts import render
-
+from django.core.mail import send_mail
 from food.models import *
 
 def index(request):
@@ -9,6 +13,22 @@ def index(request):
     return render(request, "index.html",context)
 
 def reservation(request):
+    if request.method=='POST':
+        name=request.POST['name']
+        email=request.POST['email']
+        member=request.POST['person']
+        number=request.POST['phone']
+        date=request.POST['date']
+        details=f"name={name} \n, date={date},\n perosns={member},\n contact={number}//{email}"
+        try:
+            subject = 'booking details'
+            message = details
+            email_from = settings.EMAIL_HOST_USER
+            recipient_list = ['Test@gmail.com', ]
+            send_mail( subject, message, email_from, recipient_list )
+        except:
+            print(details)
+
     return render(request, "reservation.html")
 
 def gallery(request):
@@ -22,4 +42,10 @@ def contact(request):
     return render(request, "contact.html")
 
 def menu(request):
-    return render(request, "menu.html")
+    context={
+        'All_item':Item.objects.all(),
+        'Breakfast':Item.objects.filter(category='B'),
+        'Lunch':Item.objects.filter(category='L'),
+        'Dinner':Item.objects.filter(category='D'),
+    }
+    return render(request, "menu.html",context)
